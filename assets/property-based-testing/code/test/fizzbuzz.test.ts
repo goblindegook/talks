18c80e6a9ff40d7b9d40fccd8d654b19dab4c4f4
Luis Rodrigues <goblindegook@users.noreply.github.com>
2020-02-06 15:51:33 +0000

    More fizzbuzz examples

--- assets/property-based-testing/code/test/fizzbuzz.test.ts
+++ assets/property-based-testing/code/test/fizzbuzz.test.ts
@@ -1,12 +1,12 @@
-import { range, reverse } from 'ramda'
+import { range, reverse, equals } from 'ramda'
 import { assert, property, nat } from 'fast-check'
 
-function r(a: number, b: number): number[] {
+function rrange(a: number, b: number): number[] {
   return a < b ? range(a, b + 1) : reverse(range(b, a + 1))
 }
 
 function fizzbuzz(a: number, b: number): string[] {
-  return r(a, b).map(n => {
+  return rrange(a, b).map(n => {
     if (n % 15 === 0) {
       return 'fizzbuzz'
     } else if (n % 5 === 0) {
@@ -19,6 +19,16 @@ function fizzbuzz(a: number, b: number): string[] {
 }
 
 describe('fizzbuzz', () => {
+  test('result has same length as the range provided', () => {
+    assert(
+      property(
+        nat(1000),
+        nat(1000),
+        (a, b) => fizzbuzz(a, b).length === Math.sqrt((b - a) * (b - a)) + 1
+      )
+    )
+  })
+
   test('multiples of 3 begin with fizz', () => {
     assert(
       property(
@@ -46,7 +56,7 @@ describe('fizzbuzz', () => {
     )
   })
 
-  test('other numbers are preserved', () => {
+  test('dirty: other numbers are preserved', () => {
     assert(
       property(
         nat().filter(n => n % 3 !== 0 && n % 5 !== 0),
@@ -55,11 +65,22 @@ describe('fizzbuzz', () => {
     )
   })
 
-  test('result has same length as the range provided', () => {
+  test('outputs can only be one of fizz, buzz, fizzbuzz or the number', () => {
     assert(
-      property(nat(1000), nat(1000), (a, b) => {
-        return fizzbuzz(a, b).length === Math.sqrt((b - a) * (b - a)) + 1
-      })
+      property(nat(), n =>
+        [`${n}`, 'fizz', 'buzz', 'fizzbuzz'].includes(fizzbuzz(n, n)[0])
+      )
+    )
+  })
+
+  test('output is ordered', () => {
+    assert(
+      property(nat(1000), nat(1000), (a, b) =>
+        equals(
+          fizzbuzz(a, b),
+          rrange(a, b).map(n => fizzbuzz(n, n)[0])
+        )
+      )
     )
   })
 })
