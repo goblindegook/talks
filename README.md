d629b99e6c577866b75c62083da6e1c3e9d97cb3
Luis Rodrigues <luis.rodrigues@springernature.com>
2020-10-16 20:23:04 +0100

    Switch to Reveal.js

---
+++ README.md
@@ -0,0 +1,3 @@
+# Talks
+
+[![Tests](https://github.com/goblindegook/talks/workflows/tests/badge.svg)](https://github.com/goblindegook/talks/actions)
--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { Caption } from './Caption'
-export { Mermaid } from './Mermaid'
-export { Overlay } from './Overlay'
-export { Layout, Split } from './Layout'
